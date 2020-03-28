0afc8ef93827cde93f74a26deceed37549d0767f
Lasse G. Andersen <lasse@etableret.dk>
2020-03-28 14:18:02 +0000

    Added dockerfile parser.

---
+++ lib/jocker/dockerfile.ex
@@ -0,0 +1,97 @@
+defmodule Jocker.Dockerfile do
+  import String, only: [replace: 3, split: 2, split: 3, trim: 1, to_integer: 1]
+
+  def parse(dockerfile) do
+    {:ok, tokens} = decode_file(dockerfile)
+    :ok = starts_with_from_instruction(tokens)
+    tokens
+  end
+
+  defp starts_with_from_instruction([instruction | rest]) do
+    case instruction do
+      {:arg, _} ->
+        starts_with_from_instruction(rest)
+
+      {:from, _} ->
+        :ok
+
+      {:from, _, _} ->
+        :ok
+    end
+  end
+
+  def decode_file(dockerfile) do
+    instructions =
+      dockerfile
+      # Remove escaped newlines
+      |> replace("\\\n", "")
+      # Remove empty lines
+      |> split("\n")
+      |> Enum.filter(&remove_blanks_and_comments/1)
+      |> Enum.map(&decode_line/1)
+
+    {:ok, instructions}
+  end
+
+  defp remove_blanks_and_comments(<<?#, _rest::binary>>), do: false
+  defp remove_blanks_and_comments(""), do: false
+  defp remove_blanks_and_comments(_), do: true
+
+  defp decode_line(instruction_line) do
+    [instruction, args] = split(instruction_line, " ", parts: 2)
+
+    case {instruction, args} do
+      {"USER", user} ->
+        {:user, trim(user)}
+
+      {"RUN", <<"[", _::binary>> = json_cmd} ->
+        {:run, json_decode(json_cmd)}
+
+      {"RUN", shellform} ->
+        {:run, ["/bin/sh", "-c", shellform]}
+
+      {"CMD", <<"[", _::binary>> = json_cmd} ->
+        {:cmd, json_decode(json_cmd)}
+
+      {"CMD", shellform} ->
+        {:cmd, ["/bin/sh", "-c", shellform]}
+
+      {"COPY", <<"[", _::binary>> = json_form} ->
+        {:copy, json_decode(json_form)}
+
+      {"COPY", args} ->
+        {:copy, split(args, " ")}
+
+      {"EXPOSE", port} ->
+        {:expose, to_integer(port)}
+
+      {"FROM", args} ->
+        decode_from_args(args)
+
+      {"VOLUME", args} ->
+        {:volume, args}
+
+      unknown_instruction ->
+        IO.puts("WARNING: Instruction '#{unknown_instruction}' not understood\n")
+        {:unparsed, unknown_instruction}
+    end
+  end
+
+  defp decode_from_args(args) do
+    case split(args, " AS ") do
+      ["scratch"] ->
+        {:from, "base"}
+
+      [image] ->
+        {:from, image}
+
+      [image, name] ->
+        {:from, image, name}
+    end
+  end
+
+  defp json_decode(json) do
+    {:ok, valid_json} = Jason.decode(json)
+    valid_json
+  end
+end
---
+++ test/dockerfile_test.exs
@@ -0,0 +1,51 @@
+defmodule DockerfileTest do
+  import Jocker.Dockerfile
+
+  use ExUnit.Case
+
+  test "from instruction" do
+    test1 = parse("# Testing\nFROM lol\n# One more comment")
+    test2 = parse("# Testing\nFROM lol AS maxlol\n# One more comment")
+    assert [{:from, "lol"}] == test1
+    assert [{:from, "lol", "maxlol"}] == test2
+  end
+
+  test "run instruction" do
+    test1 = parse("# Testing\nFROM lol\nRUN cat lol.txt")
+    test2 = parse("# Testing\nFROM lol\nRUN [\"/bin/sh\", \"-c\", \"cat lol.txt\"]")
+    assert [{:from, "lol"}, {:run, ["/bin/sh", "-c", "cat lol.txt"]}] == test1
+    assert [{:from, "lol"}, {:run, ["/bin/sh", "-c", "cat lol.txt"]}] == test2
+  end
+
+  test "cmd instruction" do
+    test1 = parse("# Testing\nFROM lol\nCMD cat lol.txt")
+    test2 = parse("# Testing\nFROM lol\nCMD [\"/bin/sh\", \"-c\", \"cat lol.txt\"]")
+    assert [{:from, "lol"}, {:cmd, ["/bin/sh", "-c", "cat lol.txt"]}] == test1
+    assert [{:from, "lol"}, {:cmd, ["/bin/sh", "-c", "cat lol.txt"]}] == test2
+  end
+
+  test "expose instruction" do
+    test1 = parse("# Testing\nFROM lol\nEXPOSE 1337")
+    assert [{:from, "lol"}, {:expose, 1337}] == test1
+  end
+
+  test "copy instruction" do
+    test1 = parse("# Testing\nFROM lol\nCOPY [\"lol1\", \"lol2\", \"lol3\"]")
+    test2 = parse("# Testing\nFROM lol\nCOPY lol1 lol2 lol3")
+    assert [{:from, "lol"}, {:copy, ["lol1", "lol2", "lol3"]}] == test1
+    assert [{:from, "lol"}, {:copy, ["lol1", "lol2", "lol3"]}] == test2
+  end
+
+  test "user instruction" do
+    test1 = parse("# Testing\nFROM lol\nUSER testuser")
+    test2 = parse("# Testing\nFROM lol\nUSER  testuser  ")
+    assert [{:from, "lol"}, {:user, "testuser"}] == test1
+    assert [{:from, "lol"}, {:user, "testuser"}] == test2
+  end
+
+  test "a real dockerfile" do
+    {:ok, file} = File.read("./test/data/Dockerfile")
+    instructions = parse(file)
+    assert [{:from, _} | _] = instructions
+  end
+end
