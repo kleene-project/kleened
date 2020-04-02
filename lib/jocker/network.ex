b84fb698705cfd4603163c8d11d7c0f5a9991fcb
Lasse G. Andersen <lasse@etableret.dk>
2020-04-02 18:50:24 +0000

    Added network manager module

---
+++ lib/jocker/network.ex
@@ -0,0 +1,107 @@
+defmodule Jocker.Network do
+  use GenServer
+  require Record
+
+  Record.defrecordp(:state,
+    first: :none,
+    last: :none,
+    in_use: :none,
+    if_name: :none
+  )
+
+  def start_link(range, if_name) do
+    GenServer.start_link(__MODULE__, [range, if_name], name: __MODULE__)
+  end
+
+  def new(), do: GenServer.call(__MODULE__, :new)
+
+  def remove(ip), do: GenServer.cast(__MODULE__, {:remove, ip})
+
+  ### Callback functions
+
+  @impl true
+  def init([{ip_first, ip_end}, if_name]) do
+    # Internally we convert ip-addresses to integers so they can be easily manipulated
+    state =
+      state(
+        first: ip2int(ip_first),
+        last: ip2int(ip_end),
+        in_use: MapSet.new(),
+        if_name: if_name
+      )
+
+    {"", _exitcode} = System.cmd("ifconfig", [if_name, "destroy"])
+
+    _if_name_out = if_name <> "\n"
+    {_if_name_out, 0} = System.cmd("ifconfig", ["lo", "create", "name", if_name])
+
+    Process.flag(:trap_exit, true)
+    {:ok, state}
+  end
+
+  @impl true
+  def handle_call(:new, _from, state(first: first) = state) do
+    {new_ip, in_use} = new_ip(first, state)
+    {:reply, new_ip, state(state, in_use: in_use)}
+  end
+
+  @impl true
+  def handle_cast({:remove, ip}, state) do
+    new_in_use = remove_ip(ip, state)
+    {:noreply, state[[in_use: new_in_use]]}
+  end
+
+  @impl true
+  def terminate(_reason, state(if_name: if_name)) do
+    System.cmd("ifconfig", [if_name, "destroy"])
+    :ok
+  end
+
+  ### Internal functions
+  defp remove_ip(ip, state(in_use: in_use)) do
+    ip_int = ip2int(ip)
+    MapSet.delete(in_use, ip_int)
+  end
+
+  defp new_ip(next, state(last: last, in_use: ips)) when next > last do
+    {:out_of_ips, ips}
+  end
+
+  defp new_ip(next, state(in_use: in_use) = state) do
+    case MapSet.member?(in_use, next) do
+      true ->
+        new_ip(next + 1, state)
+
+      false ->
+        new_ip = int2ip(next)
+        {new_ip, MapSet.put(in_use, next)}
+    end
+  end
+
+  defp int2ip(n) do
+    int2ip_(n, 3, [])
+  end
+
+  defp int2ip_(n, 0, prev) do
+    [n | prev]
+    |> Enum.reverse()
+    |> List.to_tuple()
+    |> :inet.ntoa()
+    |> to_string()
+  end
+
+  defp int2ip_(n, order, prev) do
+    x = floor(n / pow(order))
+    n_next = n - x * pow(order)
+    int2ip_(n_next, order - 1, [x | prev])
+  end
+
+  defp ip2int(ip) do
+    {:ok, {a, b, c, d}} = ip |> to_charlist() |> :inet.parse_address()
+    d + c * pow(1) + b * pow(2) + a * pow(3)
+  end
+
+  defp pow(n) do
+    :erlang.round(:math.pow(256, n))
+  end
+end
---
+++ test/network_test.exs
@@ -0,0 +1,25 @@
+defmodule DockerfileTest do
+  use ExUnit.Case
+
+  test "generate new ips" do
+    range = {"10.13.37.1", "10.13.37.3"}
+    if_name = "jocker0"
+    {:ok, pid} = Jocker.Network.start_link(range, if_name)
+    assert Jocker.Network.new() == "10.13.37.1"
+    assert Jocker.Network.new() == "10.13.37.2"
+    assert Jocker.Network.new() == "10.13.37.3"
+    assert Jocker.Network.new() == :out_of_ips
+    GenServer.stop(pid)
+  end
+
+  test "advanced generate new ips" do
+    range = {"10.13.37.254", "10.13.38.0"}
+    if_name = "jocker0"
+    {:ok, pid} = Jocker.Network.start_link(range, if_name)
+    assert Jocker.Network.new() == "10.13.37.254"
+    assert Jocker.Network.new() == "10.13.37.255"
+    assert Jocker.Network.new() == "10.13.38.0"
+    assert Jocker.Network.new() == :out_of_ips
+    GenServer.stop(pid)
+  end
+end
