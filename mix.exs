3de7034c3a69d8ec39b032b698bada99efc251cb
Lasse G. Andersen <lasse@etableret.dk>
2021-02-13 10:31:16 +0000

    needs eex in production as well

--- mix.exs
+++ mix.exs
@@ -35,7 +35,7 @@ defmodule Jocker.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger, :crypto],
+      extra_applications: [:logger, :crypto, :eex],
       mod: {Jocker.Engine.Application, []}
     ]
   end
